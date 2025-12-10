0930b984479762e838ebc359ad2cafab6429e1bf
panda1835 <lehoangphuc1820@gmail.com>
2025-12-11 01:31:34 +0700

    revert

--- next.config.ts
+++ next.config.ts
@@ -8,18 +8,6 @@ const nextConfig: NextConfig = {
     formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'inaturalist-open-data.s3.amazonaws.com',
-        pathname: '/photos/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.inaturalist.org',
-        pathname: '/photos/**',
-      },
-    ],
   },
 
   // Security and SEO headers
